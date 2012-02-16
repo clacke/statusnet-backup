f56150bd5788faa24e6d8c77fcabd6bc5eef2ecf
Samat K Jain <samat@samat.org>
2012-02-15 17:52:49 -0700

    Hack for more Unicode problems
    I think there was a bug in python-requests w.r.t. Unicode handling. This
    commit's hack ignores/sidesteps the issue in a very hackish (but working
    way).
    
    Thanks john@status.wjsullivan.net for reporting. See:
    
      http://identi.ca/conversation/90371594
    
    for more details.

--- StatusNet-Backup.py
+++ StatusNet-Backup.py
@@ -125,7 +125,10 @@ def main():
         if format == 'atom':
             # Fix for: ValueError: Unicode strings with encoding declaration are not supported
             # Dear lxml and Python: go die in a fire w/ your Unicode idiocy, thanks
-            raw_document = raw_document.encode('utf-8')
+            try:
+                raw_document = raw_document.encode('utf-8')
+            except UnicodeDecodeError:
+                pass
             document = etree.fromstring(raw_document)
         elif format == 'as':
             document = json.loads(raw_document)
